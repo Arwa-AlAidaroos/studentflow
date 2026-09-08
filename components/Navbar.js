0d060b0fa65a6445ad9ed4237ec21679cea50942
Arwa AlAidaroos <arwa.alaidaroos@gmail.com>
2026-09-08 12:27:43 +0300

    Complete mobile navigation drawer

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,13 @@
 "use client";
-
+import { useState  } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+
+
 export default function Navbar() {
+  const [isMobileOpen,setIsMobileOpen]= useState(false);
+ 
   // TODO: Build your Navbar component here
   return (
     <header className="navbar-wrapper">
@@ -13,6 +17,8 @@ export default function Navbar() {
           <span className="logo-text">Student<span className="logo-accent">Flow</span></span>
         </Link>
         {/* TODO: Add navigation links for '/', '/courses', '/tasks', '/resources' and a link button for '/tasks/new' */}
+         <button className="mobile-toggle-btn"onClick={() => setIsMobileOpen(!isMobileOpen)}>☰</button>
+        
         <div className="desktop-nav">
         <Link href="/" className="nav-link">Dashboard </Link>
         <Link href="/courses" className="nav-link">Courses </Link>
@@ -23,6 +29,17 @@ export default function Navbar() {
           <Link href="/tasks/new" className="btn btn-primary" > + New Task</Link>
         </div>
       </div>
+      {isMobileOpen && (<div className="mobile-nav-menu">
+            <div className="mobile-nav-links">
+              <Link href="/" className="mobile-nav-link">Dashboard </Link>
+              <Link href="/courses" className="mobile-nav-link">Courses </Link>
+              <Link href="/tasks" className="mobile-nav-link">Tasks </Link>
+              <Link href="/resources" className="mobile-nav-link">Resources </Link>
+              </div>
+              <div className="mobile-create-btn">
+              <Link href="/tasks/new" className="btn btn-primary" > + New Task</Link>
+              </div>
+          </div>)}
     </header>
   );
 }
